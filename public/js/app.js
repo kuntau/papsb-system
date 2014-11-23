2d8d008e69b6df4f4cbd6b0c0640005072bdfd24
Kuntau <kuntau17@gmail.com>
2014-11-24 00:03:14 +0800

    get rid of app variable, use pipe instead

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,44 @@
 // public/js/app.js
 
 // create the module and name it papsb
-var app = angular.module('papsb', ['ui.bootstrap', 'ui.router', 'ngAnimate', 'toastr']);
+angular.module('papsb', ['ui.bootstrap', 'ui.router', 'ngAnimate', 'toastr'])
 
 // configure our routes
 
 // create the controller and inject Angular's $scope
-app.controller('mainController', ['$scope', function($scope) {
+  .controller('ShellCtrl', ['$scope', function() {
 
+  vm = this;
   // create a message to display in our view
-  $scope.message = 'Everyone come and see how good id look';
-  $scope.sidebar = false;
-}]);
+  vm.message = 'Everyone come and see how good id look';
+  vm.sidebarStatus = 'sidebar-hidden';
 
-app.controller('aboutController', function ($scope) {
-  $scope.message = 'This is about page!';
-});
-
-app.controller('contactController', function ($scope) {
-  $scope.message = 'Contact us!!';
-});
-
-app.directive('appLogin', function () {
-  return {
-    restrict: 'A',
-    link: function(scope, el, attrs) {
-      // console.log('directive app-login');
-      el.bind('mouseup', function () {
-        console.log('done');
-        $('#papsbLogin').modal('toggle');
-      })
-    }
-    //templateUrl: 'views/login.html'
-    // template: 'Login'
+  vm.sidebarToggle = function () {
+    if (vm.sidebarStatus) {
+      vm.sidebarStatus = "";
+    } else { vm.sidebarStatus = 'sidebar-hidden' }
   }
-})
+  }])
+
+  .controller('aboutController', function ($scope) {
+    $scope.message = 'This is about page!';
+  })
+
+  .controller('contactController', function ($scope) {
+    $scope.message = 'Contact us!!';
+  })
+
+  .directive('appLogin', function () {
+    return {
+      restrict: 'A',
+      link: function(scope, el, attrs) {
+        // console.log('directive app-login');
+        el.bind('mouseup', function () {
+          console.log('done');
+          $('#papsbLogin').modal('toggle');
+        })
+      }
+      //templateUrl: 'views/login.html'
+      // template: 'Login'
+    }
+  });
