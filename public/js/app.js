30262c46b7a961b5df7dc8aa3868b9264e93edd0
Kuntau <kuntau17@gmail.com>
2015-05-12 03:32:44 +0800

    temp commit for merge

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-/// <reference path="../../typings/angularjs/angular.d.ts"/>
 // public/js/app.js
 
 // create the module and name it papsb
@@ -7,7 +6,7 @@ angular
           'ui.bootstrap',
           'ui.router',
           'ngAnimate',
-          'toastr'
+          'toastr',
           ])
   .controller('ShellCtrl', ShellCtrl)
   .controller('WorkshopCtrl', WorkshopCtrl)
@@ -15,17 +14,10 @@ angular
   .controller('ContactCtrl', ContactCtrl)
   .factory('UIService', UIService);
 
-ShellCtrl.$inject = ['UIService'];
-
-function papsbConfig(toastrConfig) {
-  angular.extend(toastrConfig, {
-    positionClass: 'toast-bottom-right',
-    tapToDismiss: true
-  });
-};
+// ShellCtrl.$inject = ['UIService'];
 
 function ShellCtrl(UIService) {
-  shell = this;
+  var shell = this;
   // create a message to display in our view
   shell.message        = 'Everyone come and see how good id look';
   shell.sidebarStatus  = UIService.getSidebarStatus();
@@ -37,16 +29,31 @@ function ShellCtrl(UIService) {
     } else { shell.sidebarStatus = 'sidebar-hidden' }
   }
 };
+// WorkshopCtrl.$inject = ['ShellCtrl'];
 
-WorkshopCtrl.$inject = ['UIService'];
-
-function WorkshopCtrl(ShellCtrl) {
-  vm = this;
+function WorkshopCtrl(toastr) {
   console.log("WorkshopCtrl");
+
+  var vm = this;
+  vm.sidebarStatus = '';
+  vm.workshopStatus = true;
+
+  var onEnter = function () {
+    ShellCtrl.openSidebar;
+  }
+
+  function onEnter() {
+    ShellCtrl.openSidebar();
+    console.log("openSidebar");
+  };
+
+  function onExit() {
+    ShellCtrl.closeSidebar();
+  };
 };
 
 function AboutCtrl() {
-  vm = this;
+  var vm = this;
   vm.message = 'This is about page!';
 };
 
@@ -57,11 +64,25 @@ function ContactCtrl($scope) {
 UIService.$inject = ['$rootScope', '$state'];
 
 function UIService($rootScope, $state) {
+  var shell = this;
   var sidebarStatus = 'sidebar-hidden';
+  var sidebarStatus = '';
   var workshopStatus = false;
-  console.log("From: UIService");
+  console.log("From: UIService/ShellCtrl");
+  // create a message to display in our view
+  shell.message        = 'Everyone come and see how good id look';
+  shell.sidebarStatus  = 'sidebar-hidden';
+  shell.workshopStatus = false;
+
+  shell.sidebarToggle = function () {
+    if (shell.sidebarStatus) {
+      shell.sidebarStatus = "";
+    } else { shell.sidebarStatus = 'sidebar-hidden' }
+  }
 
   var service =  {
+    openSidebar:        openSidebar,
+    closeSidebar:       closeSidebar,
     getSidebarStatus:   getSidebarStatus,
     setSidebarStatus:   setSidebarStatus,
     getWorkshopStatus:  getWorkshopStatus,
@@ -71,23 +92,36 @@ function UIService($rootScope, $state) {
 
   return service;
 
+  function openSidebar() {
+    console.log("opening sidebar... ");
+    shell.sidebarStatus = '';
+    shell.workshopStatus = true;
+    return;
+  };
+  function closeSidebar() {
+    console.log("closing sidebar... ");
+    shell.sidebarStatus = 'sidebar-hidden';
+    shell.workshopStatus = false;
+    return;
+  };
   function getSidebarStatus() {
-    // console.log("getSidebarStatus");
+    console.log("getSidebarStatus: " + sidebarStatus);
     return sidebarStatus;
   };
   function setSidebarStatus(status) {
-    // console.log("setSidebarStatus");
+    console.log("setSidebarStatus: " + status);
     sidebarStatus = status;
   };
   function getWorkshopStatus() {
-    // console.log("getWorkshopStatus");
+    console.log("getWorkshopStatus: " + workshopStatus);
     return workshopStatus;
   };
   function setWorkshopStatus(status) {
-    // console.log("setWorkshopStatus");
+    console.log("setWorkshopStatus: " + status);
     workshopStatus = status;
   };
   function getSidebarState() {
     return false;
   };
 }
+
