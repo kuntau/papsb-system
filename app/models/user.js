5d783e18fa441559f4fa10e118b38f7ffbb32924
Kuntau <kuntau17@gmail.com>
2015-05-21 11:26:54 +0800

    change schema
    - Schema naming now use TitleCase
    - Apparently we can't use `id` as a key. TODO: Need to research more about this, too tired right now.

--- app/models/user.js
+++ app/models/user.js
@@ -1,27 +1,25 @@
+/* user schema */
 
-var mongoose         = require('mongoose'),
-    bcrypt           = require('bcrypt-nodejs');
+var mongoose = require('mongoose'),
+    bcrypt   = require('bcrypt-nodejs');
 
-var userSchema       = mongoose.Schema({
+var UserSchema = mongoose.Schema({
 
-  local : {
-      id          : Number,
-      email       : String,
-      password    : String,
-      firstName   : String
-  }
+  username: String,
+  password: String,
+  email   : String
 
 });
 
 // generating a hash
-userSchema.methods.generateHash = function (password) {
+UserSchema.methods.generateHash = function (password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function (password) {
-  return bcrypt.compareSync(password, this.local.password);
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
 };
 
 // create model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
